1beb8bf44d3be8ad1f0b03f2f4d50ad3a57a4a4c
DIWAKAR SINGH <diwakar.singh@walgreens.com>
2023-05-24 20:33:33 +0000

    Merged PR 199833: Extract Drug Metadata using NDC Code
    This PR creates some utility functions to extract drug metadata information using the NDC Code.
    
    Usage:
    ```
    parse_drug_metadata(ndc_code="00093-0928-06_6926B4E5")
    >> {'Imprint': 'TEVA 928; 93 928',
     'Color': 'Green',
     'Shape': 'Round',
     'Strength': '40 mg',
     'Name': 'Lovastatin'}
    ```
    
    Fix typo in tools/data_tools.py.
    
    Replace scp with rsync; check time and update.
    
    Add upload functions to data tools.
    
    Correct instruction in push_to_remote in data_tools.py.

--- rx_connect/tools/data_tools.py
+++ rx_connect/tools/data_tools.py
@@ -1,11 +1,13 @@
 import subprocess
 from pathlib import Path
+from typing import Union
 
 
 def fetch_from_remote(
     remote_path: str,
     server_ip: str = "10.231.51.79",
-    local_cashe_folder: str = "local_cache",
+    local_cache_folder: str = "local_cache",
+    ignore_exist=False,
 ) -> str:
     """
     Fetch data from remote server if not yet cached.
@@ -17,18 +19,58 @@ def fetch_from_remote(
 
 
     Args:
-        remote_path (str): Full path or relative path (to home) of the remote file.
+        remote_path (str): Full path or relative path (to home) of the remote file/folder.
         server_ip (str): The remote server IP address. Default to the AI Lab GPU server at 10.231.51.79.
-        local_cashe_folder (str): The desired name of the caching folder.
+        local_cache_folder (str): The desired name of the caching folder.
+        ignore_exist (bool): Enforce rsync checking time stamp if file/folder already exists.
 
     Returns:
         str: Local file name after caching.
     """
-    Path(local_cashe_folder).mkdir(exist_ok=True)
+    Path(local_cache_folder).mkdir(exist_ok=True)
 
-    local_path = Path(local_cashe_folder + "/" + Path(remote_path).name)
-    if not local_path.exists():
-        subprocess.run(["scp", "-r", f"{server_ip}:{remote_path}", local_path])
-    assert local_path.exists()
+    local_path = Path(local_cache_folder + "/" + Path(remote_path).name)
+    if not local_path.exists() or ignore_exist:
+        subprocess.run(["rsync", "-ruq", f"{server_ip}:{remote_path}", local_path])
+    assert local_path.exists(), f"Error: Failed to cache {remote_path} from {server_ip}."
 
     return str(local_path)
+
+
+def push_to_remote(
+    local_path: Union[str, Path],
+    remote_storage_folder: str,
+    server_ip: str = "10.231.51.79",
+) -> str:
+    """
+    Push local data to remote server.
+
+    Note: requires password-less SSH. Steps:
+        1. $ ssh-keygen
+            (you can hit <enter> to the end)
+        2. $ ssh-copy-id <user_name>@10.231.51.79
+
+
+    Args:
+        local_path (str or Path): Full path or relative path (to current) of the local file/folder.
+        remote_storage_folder (str): The remote parent folder to put the file/folder in.
+        server_ip (str): The remote server IP address. Default to the AI Lab GPU server at 10.231.51.79.
+
+    Returns:
+        str: Remote file/folder name after uploading.
+    """
+    local_path_obj = Path(local_path) if isinstance(local_path, str) else local_path
+    assert local_path_obj.exists(), f"Error: {local_path_obj} not found."
+    remote_folder_exist = (
+        subprocess.run(
+            ["ssh", server_ip, f"test -e {remote_storage_folder} && test -d {remote_storage_folder}"],
+            capture_output=True,
+        ).returncode
+        == 0
+    )
+    assert remote_folder_exist, f"Error: Remote folder {remote_storage_folder} doesn't exist on {server_ip}."
+
+    remote_path = remote_storage_folder + "/" + local_path_obj.name
+    subprocess.run(["rsync", "-rq", local_path_obj, f"{server_ip}:{remote_path}"])
+
+    return remote_path
---
+++ rx_connect/tools/extract_metadata.py
@@ -0,0 +1,96 @@
+from typing import List, Optional, TypedDict
+
+import requests
+from bs4 import BeautifulSoup
+
+_TAGS: List[str] = ["Imprint", "Color", "Shape", "Strength"]
+
+
+class DrugMetadata(TypedDict, total=False):
+    Name: Optional[str]
+    Imprint: Optional[str]
+    Color: Optional[str]
+    Shape: Optional[str]
+    Strength: Optional[str]
+
+
+def parse_drug_info(ndc_code: str) -> Optional[BeautifulSoup]:
+    """Fetches drug information from drugs.com based on the NDC code.
+
+    Args:
+        ndc_code (str): The NDC (National Drug Code) of the drug.
+
+    Returns:
+        Optional[BeautifulSoup]: Parsed HTML content as a BeautifulSoup object,
+            or None if there was an error accessing the website.
+    """
+    soup: Optional[BeautifulSoup] = None
+    try:
+        url = f"https://www.drugs.com/imprints.php?ndc={ndc_code}"
+        response = requests.get(url)
+        response.raise_for_status()  # Raises exception for non-200 status codes
+
+        soup = BeautifulSoup(response.content, "html.parser")
+    except requests.RequestException as e:
+        print("Error accessing the website:", e)
+
+    return soup
+
+
+def parse_drug_name(soup: BeautifulSoup) -> Optional[str]:
+    """Extracts the drug name from the provided BeautifulSoup object.
+
+    Args:
+        soup (BeautifulSoup): Parsed HTML content as a BeautifulSoup object.
+
+    Returns:
+        Optional[str]: The drug name if found, or None if not found.
+    """
+    a_tag = soup.find("a", {"class": "ddc-text-size-small"})
+    drug_name = a_tag.text if a_tag is not None else None
+
+    return drug_name
+
+
+def parse_tags(soup: BeautifulSoup, string: str) -> Optional[str]:
+    """
+    Extracts unique tag values from the provided BeautifulSoup object based on
+    the given string.
+
+    Args:
+        soup (BeautifulSoup): Parsed HTML content as a BeautifulSoup object.
+        string (str): The string used to filter the tags.
+
+    Returns:
+        A string containing unique tag values joined with "; ", or None if no tags
+        were found.
+    """
+    s_tags = soup.find_all("dl", {"class": "ddc-text-size-small"})
+    values: List[str] = []
+    for tag in s_tags:
+        tag = tag.find("dt", string=string)
+        if tag:
+            value = tag.find_next_sibling("dd").text
+            values.append(value)
+
+    return "; ".join(set(values)) if len(values) > 0 else None
+
+
+def parse_drug_metadata(ndc_code: str) -> DrugMetadata:
+    """Parses drug metadata based on the NDC code.
+
+    Args:
+        ndc_code (str): The NDC (National Drug Code) of the drug.
+
+    Returns:
+        A dictionary containing the parsed drug metadata. The keys are the metadata
+        tags, and the values are the corresponding values.
+    """
+    metadata: DrugMetadata = {}
+
+    soup = parse_drug_info(ndc_code)
+    if soup is not None:
+        metadata = {tag: parse_tags(soup, tag) for tag in _TAGS}  # type: ignore
+        metadata["Name"] = parse_drug_name(soup)
+
+    return metadata
