6879cb155e96b446f52793f75fe6a461a225f121
Farzane TAKBIRI <zeinab.takbiri@walgreens.com>
2023-08-03 21:57:23 +0000

    Merged PR 212199: Added Method 2 A & B
    Related work items: #1133582, #1133593, #1136046

--- rx_connect/verification/similarity_eval.py
+++ rx_connect/verification/similarity_eval.py
@@ -1,15 +1,18 @@
 import random
 from pathlib import Path
+from typing import List, Tuple
 
 import click
 import numpy as np
 import pandas as pd
-from skimage.io import imread
 
 from rx_connect.pipelines.detection import RxDetection
 from rx_connect.pipelines.image import RxVision
 from rx_connect.pipelines.segment import RxSegmentation
 from rx_connect.pipelines.vectorizer import RxVectorizerSift
+from rx_connect.tools.logging import setup_logger
+
+logger = setup_logger()
 
 """_summary_
 
@@ -31,7 +34,7 @@ Step #4: calculate the binary classification metrics for evaluation report (reca
 
 return: the recall/precision / and F1-score
 
-Method #2------------------------------:
+Method #2 A & B------------------------------:
 
 This analysis aims to calculate the average probability differences
 between the model's predicted pill similarity and the corresponding labels (0 or 1).
@@ -64,25 +67,24 @@ How to test:
 
 
 def load_pill_images_and_references(
-    epill_path: str, epill_ref_list: list, images_dir: str, row: str
-) -> tuple:
+    epill_path: str, epill_ref_list: List[str], images_dir: str, row: str, nfalse_ref: int
+) -> Tuple[str, List[str], str]:
     # reading the file and its reference from the csv file
     reference_pills_true, image_name = Path(row[1]).name, row[0]
 
     # generate a false reference pill for the pill tray
-    reference_pills_false = reference_pills_true
-    while reference_pills_false == reference_pills_true:
-        reference_pills_false = random.choice(epill_ref_list)
+
+    refname_false = []
+    for _ in range(nfalse_ref + 1):
+        reference_pills_false = reference_pills_true
+        while reference_pills_false == reference_pills_true:
+            reference_pills_false = random.choice(epill_ref_list)
+        refname_false.append(f"{epill_path}/{reference_pills_false}")
 
     refname_true = f"{epill_path}/{reference_pills_true}"
-    refname_false = f"{epill_path}/{reference_pills_false}"
-    image_name = f"{images_dir}/{image_name}"
+    im_path = f"{images_dir}/{image_name}"
 
-    reference_true_image = imread(refname_true)
-    reference_false_image = imread(refname_false)
-    pill_image = imread(image_name)
-
-    return (reference_true_image, reference_false_image, pill_image)
+    return (refname_true, refname_false, im_path)
 
 
 @click.command()
@@ -101,10 +103,20 @@ def load_pill_images_and_references(
 @click.option(
     "-t",
     "--threshold",
+    default=0.8,
+    show_default=True,
     type=float,
     help="similarity confidence score to binarizing the probability",
 )
-def main(epill_path: str, data_dir: str, threshold: float) -> None:
+@click.option(
+    "-n",
+    "--nfalse_ref",
+    default=10,
+    show_default=True,
+    type=int,
+    help="number of false pill references to compare each pill tray image against",
+)
+def main(epill_path: str, data_dir: str, threshold: float, nfalse_ref: int) -> tuple:
     images_dir = f"{data_dir}/images"
     csv_dir = f"{data_dir}/pill_info.csv"
 
@@ -124,51 +136,125 @@ def main(epill_path: str, data_dir: str, threshold: float) -> None:
     imageObj.set_vectorizer(vectorizerObj)
     imageObj.set_segmenter(segmentObj)
 
-    pos, true_pos, true_neg = 0, 0, 0
-
     epill_ref_list = [
         f.name for f in list(Path(epill_path).glob("*.png")) + list(Path(epill_path).glob("*.jpg"))
     ]
     df = pd.read_csv(csv_dir, header=None, delimiter=":")
+    # counting the number of positive similarity scores and number of negative similarity scores
+
+    n_pos, n_neg = 0, 0
+    prob_diff_pos_dict, prob_diff_neg_dict = {}, {}
+    prob_diff_pos_sum, prob_diff_neg_sum = 0, 0
+
+    pos, true_pos, false_pos = 0, 0, 0
 
     for _, row in df.iterrows():
-        img_true_ref, img_false_ref, image = load_pill_images_and_references(
-            epill_path, epill_ref_list, images_dir, row
+        refname_true, refname_false, im_path = load_pill_images_and_references(
+            epill_path, epill_ref_list, images_dir, row, nfalse_ref
         )
-        imageObj.load_image(image)  # load the image of the pill tray
-        imageObj.load_ref_image(img_true_ref)  # load true ref_images
-
+        imageObj.load_image(im_path)  # load the image of the pill tray
+        imageObj.load_ref_image(refname_true)  # load true ref_images
         pos_similarity_scores = imageObj.similarity_scores
+
+        """
+        please note that because we already know that we are comparing
+        the similarity score against the "true pill reference", we know that we do not have
+        any FP or TN. That's why we only count the TP
+        """
         pos += len(pos_similarity_scores)
         true_pos += sum(pos_similarity_scores > threshold)
-        prob_diff_pos = np.mean([abs(1.0 - x) for x in pos_similarity_scores])
+        n_pos += 1
 
-        imageObj.load_image(image)  # load the image of the pill tray
-        imageObj.load_ref_image(img_false_ref)  # load the false ref_images
+        logger.assertion(
+            np.all(pos_similarity_scores <= 1.0), "Some values of similarity scores is out of range."
+        )
+        # prob_diff_pos is in fact np.mean([1.0 - x for x in pos_similarity_scores])
+        prob_diff_pos = 1.00 - np.mean(pos_similarity_scores)
+        prob_diff_pos_sum += prob_diff_pos
 
-        neg_similarity_scores = imageObj.similarity_scores
-        true_neg += sum(neg_similarity_scores < threshold)
-        prob_diff_neg = np.mean([abs(0.0 - x) for x in neg_similarity_scores])
+        prob_diff_pos_dict[Path(im_path).name] = (prob_diff_pos, len(pos_similarity_scores))
 
-    Precision = true_pos / (true_pos + true_neg)
+        # for each pill tray image, we compare it against multiple randomly chosen
+        # false pill references to minimize the bias
+
+        for i in range(nfalse_ref + 1):
+            imageObj.load_image(im_path)  # load the image of the pill tray
+            imageObj.load_ref_image(refname_false[i])  # load the false ref_images
+            neg_similarity_scores = imageObj.similarity_scores
+            """
+            please note that because we already know that we are comparing
+            the similarity score against the "false pill reference", we know that we do not have
+            any TP. That's why we only count the FP. We do have TN as well
+            but it's not used in Recall/Precision, so we skipped that as well.
+            """
+            false_pos += sum(neg_similarity_scores > threshold)
+            n_neg += 1
+
+            # x in fact is abs(0.0 - x) -> prob_diff_neg = np.mean([x for x in neg_similarity_scores])
+            prob_diff_neg = np.mean(neg_similarity_scores)
+            prob_diff_neg_sum += prob_diff_neg
+            prob_diff_neg_dict[Path(im_path).name] = (prob_diff_neg, len(neg_similarity_scores))
+
+    Precision = true_pos / (true_pos + false_pos)
     Recall = true_pos / pos
+    F1_score = Precision * Recall / (Precision + Recall)
 
+    # Method 2A: We average the prob differences over all pill trays
+
+    avg_prob_diff_pos = prob_diff_pos_sum / n_pos
+    avg_prob_diff_neg = prob_diff_neg_sum / n_neg
+    prob_diff_all = (prob_diff_pos_sum + prob_diff_neg_sum) / (n_pos + n_neg)
+
+    # METHOD#2B for calculating the average probability differences
+
+    sum_p, sum_pcount = 0, 0
+    for mean_p, count_pills in prob_diff_pos_dict.values():
+        sum_p += mean_p * count_pills
+        sum_pcount += count_pills
+    prob_diff_positive = sum_p / sum_pcount
+
+    sum_n, sum_ncount = 0, 0
+    for mean_n, count_pills in prob_diff_neg_dict.values():
+        sum_n += mean_n * count_pills
+        sum_ncount += count_pills
+    prob_diff_negative = sum_n / sum_ncount
+
+    print("binary score results :")
     print(f"Precision = {Precision:.3f}")
     print(f"Recall = {Recall:.3f}")
-    print(f"F1-score = {Precision*Recall/(Precision+Recall):.3f}")
+    print(f"F1-score = {F1_score:.3f}")
 
-    # We average the prob differences over all pill trays
-    # we use length (n) as it indicates the number of images we iterated over
-
-    n = df.shape[0]
-    avg_prob_diff_pos = round(prob_diff_pos / n, 2)
-    avg_prob_diff_neg = round(prob_diff_neg / n, 2)
-    prob_diff_all = round((prob_diff_pos + prob_diff_neg) / (2 * n), 2)
-
+    print("\n")
+    print("Probability error (method # 2A)")
     print(f"avg_prob_diff_pos = {avg_prob_diff_pos:.3f}")
     print(f"avg_prob_diff_neg = {avg_prob_diff_neg:.3f}")
     print(f"avg_prob_diff_all = {prob_diff_all:.3f}")
 
+    print("\n")
+    print("Probability error (method # 2B)")
+    print(f"prob_diff_positive = {prob_diff_positive:.3f}")
+    print(f"prob_diff_negative = {prob_diff_negative:.3f}")
+
+    return (
+        Precision,
+        Recall,
+        F1_score,
+        avg_prob_diff_pos,
+        avg_prob_diff_neg,
+        prob_diff_all,
+        prob_diff_positive,
+        prob_diff_negative,
+    )
+
 
 if __name__ == "__main__":
-    main()
+    (
+        Precision,
+        Recall,
+        F1_score,
+        avg_prob_diff_pos,
+        avg_prob_diff_neg,
+        prob_diff_all,
+        prob_diff_positive,
+        prob_diff_negative,
+    ) = main()
